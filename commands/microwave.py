9f6aead8dbd490e1e2e3c2cde293f7d670ee6e7d
Peter Foley <pefoley2@verizon.net>
2013-06-10 18:19:27 -0400

    add !microwave
    fix #26

---
+++ commands/microwave.py
@@ -0,0 +1,51 @@
+import re
+import time
+from config import CHANNEL
+
+
+def do_nuke(c):
+    c.privmsg(CHANNEL, "        ____________________          ")
+    c.privmsg(CHANNEL, "     :-'     ,   '; .,   )  '-:       ")
+    c.privmsg(CHANNEL, "    /    (          /   /      \\      ")
+    c.privmsg(CHANNEL, "   /  ;'  \\   , .  /        )   \\    ")
+    c.privmsg(CHANNEL, "  (  ( .   ., ;        ;  '    ; )    ")
+    c.privmsg(CHANNEL, "   \\    ,---:----------:---,    /    ")
+    c.privmsg(CHANNEL, "    '--'     \\ \\     / /    '--'     ")
+    c.privmsg(CHANNEL, "              \\ \\   / /              ")
+    c.privmsg(CHANNEL, "               \\     /                ")
+    c.privmsg(CHANNEL, "               |  .  |               ")
+    c.privmsg(CHANNEL, "               |, '; |               ")
+    c.privmsg(CHANNEL, "               |  ,. |               ")
+    c.privmsg(CHANNEL, "               | ., ;|               ")
+    c.privmsg(CHANNEL, "               |:; ; |               ")
+    c.privmsg(CHANNEL, "      ________/;';,.',\\ ________     ")
+    c.privmsg(CHANNEL, "     (  ;' . ;';,.;', ;  ';  ;  )    ")
+
+
+def cmd(e, c, msg):
+        levels = {1: 'Whirr...',
+                  2: 'Vrrm...',
+                  3: 'Zzzzhhhh...',
+                  4: 'SHFRRRRM...',
+                  5: 'GEEEEZZSH...',
+                  6: 'PLAAAAIIID...',
+                  7: 'KKKRRRAAKKKAAKRAKKGGARGHGIZZZZ...',
+                  8: 'Insert Nuke Here',
+                  9: 'nneeeaaaooowwwwww..... BOOOOOSH BLAM KABOOM',
+                  10: 'ssh root@remote.tjhsst.edu rm -rf ~'+e.source.nick}
+        if msg == '':
+            c.privmsg(CHANNEL, 'What to microwave?')
+            return
+        match = re.match('([0-9]*) (.*)', msg)
+        if not match:
+            c.privmsg(CHANNEL, 'Power level?')
+        else:
+            level = int(match.group(1))
+            for i in range(1, level+1):
+                if i == 8:
+                    do_nuke(c)
+                else:
+                    c.privmsg(CHANNEL, levels[i])
+                time.sleep(1)
+            time.sleep(1)
+            c.privmsg(CHANNEL, 'Ding, your %s is ready.' % match.group(2))
