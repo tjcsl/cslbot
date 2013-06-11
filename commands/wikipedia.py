d627e30e88511573a03a989eb86b586a27369e28
Techie <techfilmer@techfilmer.com>
2013-06-10 22:02:25 -0300

    Update wikipedia.py

--- commands/wikipedia.py
+++ commands/wikipedia.py
@@ -2,4 +2,4 @@ from config import CHANNEL
 
 
 def cmd(e, c, msg):
-        c.privmsg(CHANNEL, 'http://en.wikipedia.org/wiki/', msg)
+        c.privmsg(CHANNEL, 'http://en.wikipedia.org/wiki/Special:Search/', msg)
