1d06fd082195a850903425dc837e9ea5714da538
Techie <techfilmer@techfilmer.com>
2013-06-10 23:23:41 -0300

    developers names.

--- commands/about.py
+++ commands/about.py
@@ -2,4 +2,4 @@ from config import CHANNEL
 
 
 def cmd(e, c, msg):
-    c.privmsg(CHANNEL, "See https://github.com/fwilson42/ircbot")
+    c.privmsg(CHANNEL, "See https://github.com/fwilson42/ircbot Developed by fwilson, pfoley, and TechFilmer. Type !help for a list of commands.")
