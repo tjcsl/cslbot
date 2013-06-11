25889fbaa49e689f8c599415446c26a893a03a3e
Samuel Damashek <samuel.damashek@gmail.com>
2013-06-11 16:30:59 -0400

    Various improvements to !google

--- commands/google.py
+++ commands/google.py
@@ -4,11 +4,12 @@ import os
 import re
 
 def cmd(e, c, msg):
-	url = urllib.quote(msg)
-	f = os.popen('lynx --head --dump "http://www.google.com/search?q=%s&btnI=I%%27m+Feeling+Lucky"' % url)
-	output = f.read()
-	location = re.findall(r"Location: (.+)", output)
-	if len(location) != 1 or "http://www.google.com" in location[0]:
-		c.privmsg(CHANNEL, "Google didn't say much :(")
-		return
-	c.privmsg(CHANNEL, "Google says %s" % location[0])
+        url = urllib.quote(msg)
+        c.privmsg(CHANNEL, url)
+        f = os.popen('lynx --head --dump "http://www.google.com/search?q=%s&btnI=I%%27m+Feeling+Lucky"' % url)
+        output = f.read()
+        location = re.findall(r"Location: (.+)", output)
+        if len(location) != 1 or "http://www.google.com" in location[0]:
+                c.privmsg(CHANNEL, "Google didn't say much :(")
+        else:
+                c.privmsg(CHANNEL, "Google says %s" % location[0])
