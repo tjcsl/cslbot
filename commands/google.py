c730c09a06f82820610663e8f33234c8474ac1b8
Samuel Damashek <samuel.damashek@gmail.com>
2013-06-11 16:19:08 -0400

    Fix !google
    quote %, fix regex and add second option, fix length

--- commands/google.py
+++ commands/google.py
@@ -5,10 +5,10 @@ import re
 
 def cmd(e, c, msg):
 	url = urllib.quote(msg)
-	f = os.popen('lynx --head --dump "http://www.google.com/search?q=%s&btnI=I%27m+Feeling+Lucky"' % url)
+	f = os.popen('lynx --head --dump "http://www.google.com/search?q=%s&btnI=I%%27m+Feeling+Lucky"' % url)
 	output = f.read()
-	location = re.findall(r"^Location:\s*(.+)")
-	if location.length != 1 or "http://www.google.com" in location[0]:
+	location = re.findall(r"Location: (.+)", output)
+	if len(location) != 1 or "http://www.google.com" in location[0]:
 		c.privmsg(CHANNEL, "Google didn't say much :(")
 		return
 	c.privmsg(CHANNEL, "Google says %s" % location[0])
