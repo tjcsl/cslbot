b8dfd42044a37de0ab742ac11b7059dae59a65cf
James Forcier <csssuf724@gmail.com>
2013-06-11 19:59:55 -0400

    Better error checking for !fweather

--- commands/fweather.py
+++ commands/fweather.py
@@ -1,9 +1,15 @@
 from config import CHANNEL
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import socket
 
 def cmd(e, c, msg):
         html = urlopen('http://thefuckingweather.com/?where=' + msg.replace(' ', '%20'), timeout=5).read()
         soup = BeautifulSoup(html)
         temp, remark, flavor = soup.findAll('p')
-        c.privmsg(CHANNEL, str(temp.contents[0].contents[0])  + ' F? ' + str(remark.contents[0]))
+        try:
+            c.privmsg(CHANNEL, str(temp.contents[0].contents[0])  + ' F? ' + str(remark.contents[0]))
+        except ValueError:
+            c.privmsg(CHANNEL, 'No results for location ' + msg + '.')
+        except socket.timeout:
+            c.privmsg(CHANNEL, 'Connection timed out.')
