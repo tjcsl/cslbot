2f8f9110c478e678e71e91b68d0de6e7579ff9b4
Peter Foley <pefoley2@verizon.net>
2013-06-11 17:51:17 -0400

    make google less hacky

--- commands/google.py
+++ commands/google.py
@@ -1,15 +1,16 @@
 from config import CHANNEL
-import urllib.parse
-import os
-import re
+from urllib.parse import quote
+from urllib.request import urlopen
+import json
 
 
 def cmd(e, c, msg):
-        url = urllib.parse.quote(msg)
-        f = os.popen('lynx --head --dump "http://www.google.com/search?q=%s&btnI=I%%27m+Feeling+Lucky"' % url)
-        output = f.read()
-        location = re.findall(r"Location: (.+)", output)
-        if len(location) != 1 or "http://www.google.com" in location[0]:
-                c.privmsg(CHANNEL, "Google didn't say much :(")
-        else:
-                c.privmsg(CHANNEL, "Google says %s" % location[0])
+        # pfoley's private key -- do not abuse
+        key = 'AIzaSyAVcc0KavdZx_cfE1gtwMnsVudmJ5lvVMo'
+        searchid = '011314769761209412182:6apbsde5g8e'
+        data = json.loads(urlopen('https://www.googleapis.com/customsearch/v1?key=%s&cx=%s&q=%s' % (key, searchid, quote(msg))).read().decode())
+        try:
+            url = data['items'][0]['link']
+            c.privmsg(CHANNEL, "Google says " + url)
+        except KeyError:
+            c.privmsg(CHANNEL, "Google didn't say much")
