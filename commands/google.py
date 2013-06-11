fa33cae8a7766f156c89acdf2e96ee178e623595
Samuel Damashek <samuel.damashek@gmail.com>
2013-06-11 17:02:20 -0400

    Still not working but changed to parse for python3

--- commands/google.py
+++ commands/google.py
@@ -1,10 +1,10 @@
 from config import CHANNEL
-import urllib
+import urllib.parse
 import os
 import re
 
 def cmd(e, c, msg):
-        url = urllib.quote(msg)
+        url = urllib.parse.quote(msg)
         c.privmsg(CHANNEL, url)
         f = os.popen('lynx --head --dump "http://www.google.com/search?q=%s&btnI=I%%27m+Feeling+Lucky"' % url)
         output = f.read()
