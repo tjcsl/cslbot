9006586dbb39e7b0f7f2c66a4e2f16fe86d8677c
Samuel Damashek <samuel.damashek@gmail.com>
2013-06-11 17:26:12 -0400

    Remove testing privmsg

--- commands/google.py
+++ commands/google.py
@@ -5,7 +5,6 @@ import re
 
 def cmd(e, c, msg):
         url = urllib.parse.quote(msg)
-        c.privmsg(CHANNEL, url)
         f = os.popen('lynx --head --dump "http://www.google.com/search?q=%s&btnI=I%%27m+Feeling+Lucky"' % url)
         output = f.read()
         location = re.findall(r"Location: (.+)", output)
