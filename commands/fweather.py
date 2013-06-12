7f320affd5abc7820f2be3d7125b36c3f886876e
James Forcier <csssuf724@gmail.com>
2013-06-11 20:01:47 -0400

    Whoops, fixed typo

--- commands/fweather.py
+++ commands/fweather.py
@@ -4,10 +4,10 @@ from bs4 import BeautifulSoup
 import socket
 
 def cmd(e, c, msg):
-        html = urlopen('http://thefuckingweather.com/?where=' + msg.replace(' ', '%20'), timeout=5).read()
-        soup = BeautifulSoup(html)
-        temp, remark, flavor = soup.findAll('p')
         try:
+            html = urlopen('http://thefuckingweather.com/?where=' + msg.replace(' ', '%20'), timeout=5).read()
+            soup = BeautifulSoup(html)
+            temp, remark, flavor = soup.findAll('p')
             c.privmsg(CHANNEL, str(temp.contents[0].contents[0])  + ' F? ' + str(remark.contents[0]))
         except ValueError:
             c.privmsg(CHANNEL, 'No results for location ' + msg + '.')
