ab5fb8541ed7399c421422cabda11540649c3838
Techie <techfilmer@techfilmer.com>
2013-06-12 16:52:35 -0300

    sync with master

--- commands/award.py
+++ commands/award.py
@@ -1,4 +1,16 @@
+from random import randrange
 def cmd(send, msg, args):
         if not msg:
             return
-        send(msg + ': I hereby award you this gold medal.')
+        type = randrange(1, 5)
+        if(type == 1):
+                type = 'gold'
+        elif(type == 2):
+                type = 'silver'
+        elif(type == 3):
+                type = 'bronze'
+        elif(type == 4):
+                type = 'platinum'
+        else:
+                pass
+        send(msg + ': I hereby award you this' + type + 'medal.')
