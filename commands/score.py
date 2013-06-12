5f375afe9cff01c2d4152e20f15e21da4dc77098
Peter Foley <pefoley2@verizon.net>
2013-06-12 15:39:23 -0400

    make score return a random person

--- commands/score.py
+++ commands/score.py
@@ -1,12 +1,13 @@
 import re
 from os.path import dirname
+from random import choice
 import json
 
 
 def cmd(send, msg, args):
-    match = re.match('([a-zA-Z0-9]+)', msg)
     try:
         data = json.load(open(dirname(__file__)+"/../score"))
+        match = re.match('([a-zA-Z0-9]+)', msg)
         if match:
             name = match.group(1).lower()
             try:
@@ -14,24 +15,28 @@ def cmd(send, msg, args):
                 send("%s has %i points!" % (name, score))
             except:
                 send("Nobody cares about " + name)
-        match = re.match('--(.*)', msg)
-        if match:
-            sorted_data = sorted(data, key=data.get)
-            if match.group(1) == 'high':
-                send('High Scores:')
-                for x in reversed(range(0, 3)):
-                    try:
-                        name = sorted_data[x]
-                        send("%s: %s" % (name, data[name]))
-                    except IndexError:
-                        pass
-            if match.group(1) == 'low':
-                send('Low Scores:')
-                for x in range(0, 3):
-                    try:
-                        name = sorted_data[x]
-                        send("%s: %s" % (name, data[name]))
-                    except IndexError:
-                        pass
+        else:
+            match = re.match('--(.*)', msg)
+            if match:
+                sorted_data = sorted(data, key=data.get)
+                if match.group(1) == 'high':
+                    send('High Scores:')
+                    for x in reversed(range(0, 3)):
+                        try:
+                            name = sorted_data[x]
+                            send("%s: %s" % (name, data[name]))
+                        except IndexError:
+                            pass
+                if match.group(1) == 'low':
+                    send('Low Scores:')
+                    for x in range(0, 3):
+                        try:
+                            name = sorted_data[x]
+                            send("%s: %s" % (name, data[name]))
+                        except IndexError:
+                            pass
+            else:
+                name = choice(list(data.keys()))
+                send("%s has %i points!" % (name, data[name]))
     except OSError:
         send("Nobody cares about anything.")
