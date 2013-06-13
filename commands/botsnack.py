1dc105e60573f9ef13ddabcaf23dcd3d58cdc793
Samuel Damashek <samuel.damashek@gmail.com>
2013-06-13 09:00:43 -0400

    Capitalize, ahamilto lied
    only 1st letter capitalized

--- commands/botsnack.py
+++ commands/botsnack.py
@@ -2,4 +2,4 @@ def cmd(send, msg, args):
     if not msg:
         send("This tastes yummy!")
     else:
-        send(msg.title() + " tastes yummy!")
+        send(msg.capitalize() + " tastes yummy!")
