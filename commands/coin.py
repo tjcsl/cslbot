8c6cdbe44ecc27abd56677d0eee46912c41cc58f
Peter Foley <pefoley2@verizon.net>
2013-06-13 17:07:04 -0400

    cleanup

--- commands/coin.py
+++ commands/coin.py
@@ -1,5 +1,6 @@
-from random import randrange
+from random import choice
+
+
 def cmd(send, msg, args):
         coin = ['heads', 'tails']
-        flip = str(coin[randrange(0, 1)])
-        send('The coin lands on...' + flip + '.')
+        send('The coin lands on...' + choice(coin) + '.')
--- commands/time.py
+++ commands/time.py
@@ -1,6 +1,7 @@
 import time
-bold = '\u0002'
+
+
 def cmd(send, msg, args):
-        ctime = time.localtime()
-        send(bold + 'Date: ' + bold + str(ctime[1]) + '/' + str(ctime[2]) + '/' + str(ctime[0])
-             + bold + '   Time: ' + bold + str(ctime[3]) + ':' + str(ctime[4]) + ':' + str(ctime[5]))
+        bold = '\x02'
+        timeformat = bold + "Date: " + bold + "%d/%m/%Y" + bold + "   Time: " + bold + "%I:%M:%S"
+        send(time.strftime(timeformat))
