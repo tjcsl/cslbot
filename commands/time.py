06e7d02222e5fe03d0073a678be92cf08fdc6210
James Forcier <csssuf724@gmail.com>
2013-06-13 11:48:47 -0400

    Made !time output not look like s**t

--- commands/time.py
+++ commands/time.py
@@ -2,5 +2,5 @@ import time
 bold = '\u0002'
 def cmd(send, msg, args):
         ctime = time.localtime()
-        send(bold + 'Date:' + bold + str(ctime[1]) + '/' + str(ctime[2]) + '/' + str(ctime[0])
-             + bold + 'Time' + bold + str(ctime[3]) + ':' + str(ctime[4]) + ':' + str(ctime[5]))
+        send(bold + 'Date: ' + bold + str(ctime[1]) + '/' + str(ctime[2]) + '/' + str(ctime[0])
+             + bold + '   Time: ' + bold + str(ctime[3]) + ':' + str(ctime[4]) + ':' + str(ctime[5]))
