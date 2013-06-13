7537400fe029b957b727c8da44bdf3787de8f432
Techie <techfilmer@techfilmer.com>
2013-06-13 11:39:23 -0400

    fixes hopefully

--- commands/time.py
+++ commands/time.py
@@ -2,5 +2,5 @@ import time
 bold = '\u0002'
 def cmd(send, msg, args):
         ctime = time.localtime()
-        send(bold + 'Date:' + bold + ctime[1] + '/' + ctime[2] + '/' + ctime[0] +
-             bold + 'Time' + bold + ctime[3] + ':' + ctime[4] + ':' + ctime[5])
+        send(bold + 'Date:' + bold + ctime[1] + '/' + ctime[2] + '/' + ctime[0]
+             + bold + 'Time' + bold + ctime[3] + ':' + ctime[4] + ':' + ctime[5])
