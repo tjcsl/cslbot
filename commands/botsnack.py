0f0f293d39b3e35c2f8c57fa712cfd72db15e0af
Samuel Damashek <samuel.damashek@gmail.com>
2013-06-13 08:58:36 -0400

    Fix capitalization

--- commands/botsnack.py
+++ commands/botsnack.py
@@ -2,5 +2,4 @@ def cmd(send, msg, args):
     if not msg:
         send("This tastes yummy!")
     else:
-        msg.capitalize()
-        send(msg + " tastes yummy!")
+        send(msg.title() + " tastes yummy!")
