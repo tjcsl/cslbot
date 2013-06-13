f0d10eef97646eb2aaea5a8d86e0d4a11c1d4c53
James Forcier <csssuf724@gmail.com>
2013-06-13 11:46:10 -0400

    Fixed !time

--- commands/time.py
+++ commands/time.py
@@ -2,5 +2,5 @@ import time
 bold = '\u0002'
 def cmd(send, msg, args):
         ctime = time.localtime()
-        send(bold + 'Date:' + bold + ctime[1] + '/' + ctime[2] + '/' + ctime[0]
-             + bold + 'Time' + bold + ctime[3] + ':' + ctime[4] + ':' + ctime[5])
+        send(bold + 'Date:' + bold + str(ctime[1]) + '/' + str(ctime[2]) + '/' + str(ctime[0])
+             + bold + 'Time' + bold + str(ctime[3]) + ':' + str(ctime[4]) + ':' + str(ctime[5]))
