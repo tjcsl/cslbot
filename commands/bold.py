201a359ce3a3000fa2592334cc1c2440b60fd639
Techie <techfilmer@techfilmer.com>
2013-06-13 08:28:57 -0400

    wut, how'd that get there

--- commands/bold.py
+++ commands/bold.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-bold = \u0002
-=======
 bold = '\u0002'
->>>>>>> 670dab997667e4f42b767a1feaf67e3bff59cf39
 def cmd(send, msg, args):
         msg = bold + msg + bold
         send(msg)
