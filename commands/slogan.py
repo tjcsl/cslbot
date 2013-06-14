dc87e5d363f31fc52add2695f75a4d7114715ded
Peter Foley <pefoley2@verizon.net>
2013-06-14 14:37:13 -0400

    make slogan more reliable

--- commands/slogan.py
+++ commands/slogan.py
@@ -2,11 +2,18 @@ import re
 from urllib.request import urlopen
 
 
-def cmd(send, msg, args):
-        if not msg:
-            return
+def gen_slogan(msg):
         msg = msg.replace(' ', '%20')
         html = urlopen('http://www.sloganizer.net/en/outbound.php?slogan='
                        + msg, timeout=2).read().decode()
-        slogan = re.search('>(.*)<', html).group(1).replace('\\', '')
-        send(slogan)
+        return re.search('>(.*)<', html).group(1).replace('\\', '')
+
+
+def cmd(send, msg, args):
+    if not msg:
+        return
+
+    slogan = gen_slogan(msg)
+    while not slogan:
+        slogan = gen_slogan(msg)
+    send(slogan)
