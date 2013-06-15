558ffde7b4bdb36cc8b833b2d8997bbbee67864c
Peter Foley <pefoley2@verizon.net>
2013-06-15 15:46:58 -0400

    wyang

--- commands/botsnack.py
+++ commands/botsnack.py
@@ -5,6 +5,6 @@ def cmd(send, msg, args):
     if not msg:
         send("This tastes yummy!")
     elif msg == NICK:
-        send("Cannibalism is generally frowned upon.")
+        send("wyang says Cannibalism is generally frowned upon.")
     else:
         send(msg.capitalize() + " tastes yummy!")
