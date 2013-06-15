3fdb67e73c960ab246a0fa9c9172c15f098c053f
Peter Foley <pefoley2@verizon.net>
2013-06-15 15:24:00 -0400

    make bc better

--- commands/bc.py
+++ commands/bc.py
@@ -2,17 +2,13 @@ import subprocess
 import json
 from os.path import dirname
 
-args = ['channel']
-
 
 def cmd(send, msg, args):
         if not msg:
             return
         data = json.load(open(dirname(__file__)+"/../score"))
-        for u in args['channel'].users():
-            if u.lower() in data:
-                score = str(data[u.lower()])
-                msg = msg.replace(u, score)
+        for u in data:
+            msg = msg.replace(u, str(data[u]))
         msg += '\n'
         proc = subprocess.Popen(['bc', '-l'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         output = proc.communicate(msg.encode())[0]
