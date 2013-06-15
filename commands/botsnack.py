1f19cc55fafe9a54811a37201c2fb0b90ea54bae
Peter Foley <pefoley2@verizon.net>
2013-06-15 15:31:37 -0400

    don't snack on the bot

--- commands/botsnack.py
+++ commands/botsnack.py
@@ -1,5 +1,10 @@
+from config import NICK
+
+
 def cmd(send, msg, args):
     if not msg:
         send("This tastes yummy!")
+    elif msg == NICK:
+        send("Cannibalism is generally frowned upon.")
     else:
         send(msg.capitalize() + " tastes yummy!")
