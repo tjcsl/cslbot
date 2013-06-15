75aa495277e9a98e8892d71a8a160ea8155df7d0
Peter Foley <pefoley2@verizon.net>
2013-06-15 17:05:30 -0400

    Revert "Update pester.py"
    This reverts commit f2241061636ca9556c3f3671782e61d93584ce98.

--- commands/pester.py
+++ commands/pester.py
@@ -5,7 +5,6 @@ limit = 1
 
 def cmd(send, msg, args):
         match = re.match('([a-zA-Z0-9]+) (.*)', msg)
-        send("This command has been disabled due to abuse."); return;
         if match:
             message = match.group(2) + " "
             send('%s: %s' % (match.group(1), message * 3))
