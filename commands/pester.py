f2241061636ca9556c3f3671782e61d93584ce98
fwilson42 <foxwilson123@gmail.com>
2013-06-15 16:54:44 -0400

    Update pester.py

--- commands/pester.py
+++ commands/pester.py
@@ -5,6 +5,7 @@ limit = 1
 
 def cmd(send, msg, args):
         match = re.match('([a-zA-Z0-9]+) (.*)', msg)
+        send("This command has been disabled due to abuse."); return;
         if match:
             message = match.group(2) + " "
             send('%s: %s' % (match.group(1), message * 3))
