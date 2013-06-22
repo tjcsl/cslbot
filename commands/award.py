0dd68f6eba2badf322d563b03d7eb8046911e0b8
fwilson42 <foxwilson123@gmail.com>
2013-06-22 11:03:32 -0300

    Fix #60

--- commands/award.py
+++ commands/award.py
@@ -1,18 +1,8 @@
-from random import randrange
+from random import choice
 
 
 def cmd(send, msg, args):
         if not msg:
             return
-        type = randrange(1, 5)
-        if(type == 1):
-                type = 'gold'
-        elif(type == 2):
-                type = 'silver'
-        elif(type == 3):
-                type = 'bronze'
-        elif(type == 4):
-                type = 'platinum'
-        else:
-                pass
-        send(msg + ': I hereby award you this ' + type + ' medal.')
+        atype = choice(['gold', 'silver', 'bronze', 'platinum'])
+        send(msg + ': I hereby award you this ' + atype + ' medal.')
