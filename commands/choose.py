4218952956520b6fb1b87d5266ac3443505183e0
Peter Foley <pefoley2@verizon.net>
2013-06-25 20:36:41 -0400

    fix spacing

--- commands/choose.py
+++ commands/choose.py
@@ -5,4 +5,4 @@ args = ['nick']
 def cmd(send, msg, args):
     choices = msg.split(' ')
     action = ['draws a slip of paper from a hat and gets...', 'says enie, menie, miney, moe and chooses...']
-    send(args['nick'] + choice(action) + choice(choices))
+    send(args['nick'] + ' ' + choice(action) + ' ' + choice(choices))
