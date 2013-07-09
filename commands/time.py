ee4d0c661c6dc78d6c45e491e1dea884a691426e
Fox Wilson <2016fwilson@tjhsst.edu>
2013-07-08 22:13:30 -0400

    fix time format

--- commands/time.py
+++ commands/time.py
@@ -19,5 +19,5 @@ import time
 
 def cmd(send, msg, args):
         bold = '\x02'
-        timeformat = bold + "Date: " + bold + "%d/%m/%Y" + bold + "   Time: " + bold + "%I:%M:%S"
+        timeformat = bold + "Date: " + bold + "%m/%d/%Y" + bold + "   Time: " + bold + "%I:%M:%S"
         send(time.strftime(timeformat))
