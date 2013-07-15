0529170cbdecec50043ef5076c68361b53754c50
Fox Wilson <2016fwilson@tjhsst.edu>
2013-07-15 16:16:41 -0400

    server escape sequence

--- server.py
+++ server.py
@@ -24,6 +24,7 @@ Copyright (c) 2013 Fox Wilson, Peter Foley, Srijay Kasturi, Samuel Damashek, and
 Licensed under the GNU GPL version 2.
 
 Type "help" for a list of commands.
+\xff\xfb\x01
 """
 HELP = """
 == IRCbot console commands list ==
@@ -47,7 +48,7 @@ class BotNetHandler(socketserver.BaseRequestHandler):
     def handle(self):
         send = lambda msg: self.request.send(msg.encode())
         recv = lambda: self.request.recv(1024).decode().strip()
-        send("Password: ")
+        send("Password: " + '\xff\xfe\x01')
         if recv() == CTRLPASS:
             send(WELCOME)
         else:
