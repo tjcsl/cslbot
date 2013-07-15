96b2723dbe7720e7149bc02b8921b39f843e48d0
Fox Wilson <2016fwilson@tjhsst.edu>
2013-07-15 16:22:05 -0400

    TypeError: __init__() got an unexpected keyword argument 'daemon'

--- server.py
+++ server.py
@@ -24,7 +24,6 @@ Copyright (c) 2013 Fox Wilson, Peter Foley, Srijay Kasturi, Samuel Damashek, and
 Licensed under the GNU GPL version 2.
 
 Type "help" for a list of commands.
-\xff\xfb\x01
 """
 HELP = """
 == IRCbot console commands list ==
@@ -37,7 +36,7 @@ quit: quit the console session
 def init_server(bot):
     server = BotNetServer(('', SERVERPORT), BotNetHandler)
     server.bot = bot
-    threading.Thread(target=server.serve_forever, daemon=True).start()
+    threading.Thread(target=server.serve_forever).start()
 
 
 class BotNetServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
@@ -48,7 +47,7 @@ class BotNetHandler(socketserver.BaseRequestHandler):
     def handle(self):
         send = lambda msg: self.request.send(msg.encode())
         recv = lambda: self.request.recv(1024).decode().strip()
-        send("Password: " + '\xff\xfe\x01')
+        send("Password: ")
         if recv() == CTRLPASS:
             send(WELCOME)
         else:
