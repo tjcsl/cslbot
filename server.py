3c7fc68a72021439ed88fc36c5ac98f046aef9c3
Peter Foley <pefoley2@verizon.net>
2013-07-15 21:04:35 -0400

    server improvements

--- server.py
+++ server.py
@@ -46,36 +46,49 @@ class BotNetServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
 
 
 class BotNetHandler(socketserver.BaseRequestHandler):
+    def get_data(self):
+        SIZE = 4096
+        msg = ""
+        while True:
+            data = self.request.recv(SIZE).decode()
+            msg += data
+            if len(data) != SIZE:
+                break
+        return msg
+
     def handle(self):
         send = lambda msg: self.request.send(msg.encode())
-        recv = lambda: self.request.recv(1024).decode().strip()
         send("Password: ")
-        if recv() == CTRLPASS:
+        msg = self.get_data().splitlines()
+        if msg[0].strip() == CTRLPASS:
             send(WELCOME)
         else:
             send("Incorrect password.\n")
             self.request.close()
             return
+        if len(msg) > 1:
+            msg = list(reversed(msg[1:]))
+            end = len(msg)
         while True:
-            try:
-                send("ircbot> ")
-            except OSError:
-                # connection has been closed
-                return
-            try:
-                cmd = recv()
-            except:
-                # connection has been closed (ctrl+c or similar)
-                return
+            if end:
+                cmd = msg[end-1].strip()
+                end -= 1
+            else:
+                try:
+                    send("ircbot> ")
+                    cmd = self.get_data().strip()
+                except OSError:
+                    # connection has been closed
+                    return
             if cmd == "help":
                 send(HELP)
             elif cmd == "admins":
                 admins = ", ".join(self.server.bot.handler.admins.keys())
-                send(admins+'\n')
+                send(admins + '\n')
             elif cmd == "raw":
                 while cmd != "endraw":
                     send("ircbot-raw> ")
-                    cmd = recv()
+                    cmd = self.get_data().strip()
                     if cmd == "endraw":
                         break
                     self.server.bot.handler.connection.send_raw(cmd)
