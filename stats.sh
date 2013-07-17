8323c29d61df7d83a26a142f150bf37dd543040f
Peter Foley <pefoley2@verizon.net>
2013-07-17 08:48:39 -0400

    make stats better

--- stats.sh
+++ stats.sh
@@ -1,3 +1,3 @@
-for i in `find -name '*.py'`; do cat $i | wc -l; done > lines.txt
-echo `python -c "print sum([int(i) if i != '' else 0 for i in open('lines.txt').read().split('\n')])"` lines of code in IRCbot
-rm lines.txt
+files=`find -name '*.py'`
+lines=`wc -l $files | tail -1 | sed s/total//`
+echo $lines lines of code in IRCbot
