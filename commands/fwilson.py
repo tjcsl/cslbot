6ac216cf5b7beb7fd9c386d4e92617ecca066f68
Samuel Damashek <samuel.damashek@gmail.com>
2013-07-17 18:44:28 -0300

    not use gen_creffett, just use upper

--- commands/fwilson.py
+++ commands/fwilson.py
@@ -14,7 +14,6 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-from creffett import gen_creffett
 
 def cmd(send, msg, args):
-    send(gen_creffett("wheeeee " + msg))
+    send(("wheeeee " + msg).upper())
