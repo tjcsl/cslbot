ff1fca0c943ad193da9a2766bd43c9dfc459f58d
Peter Foley <pefoley2@verizon.net>
2013-07-20 19:07:47 -0400

    make pfoley better :)

--- commands/pfoley.py
+++ commands/pfoley.py
@@ -14,6 +14,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+args = ['modules']
 
 def cmd(send, msg, args):
-    send(msg + " :)")
+    slogan = args['modules']['slogan'].gen_slogan(msg)
+    send(slogan + " :)")
