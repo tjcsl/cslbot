ca4e4db84c48595b65fcde544c4424ba5f7f3541
Peter Foley <pefoley2@verizon.net>
2013-07-20 14:35:36 -0400

    fix #128 default weather location

--- commands/weather.py
+++ commands/weather.py
@@ -14,39 +14,72 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+import json
+import re
+from config import WEATHERAPIKEY
 from urllib.request import urlopen
 from urllib.parse import quote
-import json
 
+args = ['nick', 'datadir']
+
+def get_default(nick, prefsfile, send):
+    try:
+        defaults = json.load(open(prefsfile))
+        location = defaults[nick]
+    except (OSError, KeyError):
+        send("No default location for %s, defaulting to TJ." % nick)
+        # default to TJHSST
+        location = '22312'
+    return location
+
+def set_default(nick, location, prefsfile, send):
+    try:
+        defaults = json.load(open(prefsfile))
+    except OSError:
+        defaults = {}
+    if get_weather(location, send):
+        send("Setting default location")
+        defaults[nick] = location
+    f = open(prefsfile, "w")
+    json.dump(defaults, f)
+    f.write("\n")
+    f.close()
+
+def get_weather(msg, send):
+    msg = quote(msg)
+    html = urlopen('http://api.wunderground.com/api/%s/conditions/q/%s.json'
+                   % (WEATHERAPIKEY, msg), timeout=1).read().decode()
+    forecasthtml = urlopen('http://api.wunderground.com/api/%s/forecast/q/%s.json'
+                           % (WEATHERAPIKEY, msg), timeout=1).read().decode()
+    data = json.loads(html)
+    if 'current_observation' in data:
+        data = data['current_observation']
+    else:
+        send("Invalid or Ambiguous Location")
+        return False
+    forecastdata = json.loads(forecasthtml)['forecast']['simpleforecast']['forecastday'][0]
+    send("Current weather for %s:" % data['display_location']['full'])
+    current = '%s, Temp: %s, Humidity: %s, Pressure: %s", Wind: %s' % (
+        data['weather'],
+        data['temp_f'],
+        data['relative_humidity'],
+        data['pressure_in'],
+        data['wind_string'])
+    forecast = '%s, High: %s, Low: %s' % (
+        forecastdata['conditions'],
+        forecastdata['high']['fahrenheit'],
+        forecastdata['low']['fahrenheit'])
+    send(current)
+    send("Forecast: " + forecast)
+    return True
 
 def cmd(send, msg, args):
-        # pfoley's api key -- do not abuse
-        apikey = 'c685cf5a0aa40e4f'
-        if not msg:
-            # default to TJHSST
-            msg = '22312'
-        msg = quote(msg)
-        html = urlopen('http://api.wunderground.com/api/%s/conditions/q/%s.json'
-                       % (apikey, msg), timeout=1).read().decode()
-        forecasthtml = urlopen('http://api.wunderground.com/api/%s/forecast/q/%s.json'
-                               % (apikey, msg), timeout=1).read().decode()
-        data = json.loads(html)
-        if 'current_observation' in data:
-            data = data['current_observation']
-        else:
-            send("Invalid or Ambiguous Location")
-            return
-        forecastdata = json.loads(forecasthtml)['forecast']['simpleforecast']['forecastday'][0]
-        send("Current weather for %s:" % data['display_location']['full'])
-        current = '%s, Temp: %s, Humidity: %s, Pressure: %s", Wind: %s' % (
-            data['weather'],
-            data['temp_f'],
-            data['relative_humidity'],
-            data['pressure_in'],
-            data['wind_string'])
-        forecast = '%s, High: %s, Low: %s' % (
-            forecastdata['conditions'],
-            forecastdata['high']['fahrenheit'],
-            forecastdata['low']['fahrenheit'])
-        send(current)
-        send("Forecast: " + forecast)
+    prefsfile = args['datadir'] + "weather"
+    match = re.match("set (.*)", msg)
+    if match:
+        set_default(args['nick'], match.group(1), prefsfile, send)
+        return
+    if not msg:
+        defaults = args['datadir'] + 'weather'
+        msg = get_default(args['nick'], prefsfile, send)
+    get_weather(msg, send)
