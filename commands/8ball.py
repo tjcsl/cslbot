9a99c397c83c4428c2785b11af3425f1d6a5c4d9
Peter Foley <pefoley2@verizon.net>
2013-07-23 17:34:41 -0400

    spelling

--- commands/8ball.py
+++ commands/8ball.py
@@ -23,6 +23,6 @@ def cmd(send, msg, args):
             send("What is your question?")
             return
         answers = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes, definitely', 'You may rely on it', 'As I see it, yes', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes',
-                   'Reply hazy, try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrace and ask again',
+                   'Reply hazy, try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again',
                    'Don\'t count on it', 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful']
         send('%s says... %s' % (NICK, choice(answers)))
