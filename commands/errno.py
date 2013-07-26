54b9f4698226056feca24cd7dddd56e72840cfa8
Peter Foley <pefoley2@verizon.net>
2013-07-25 20:12:29 -0400

    fix #132

--- commands/errno.py
+++ commands/errno.py
@@ -22,5 +22,10 @@ from random import choice
 def cmd(send, msg, args):
         errno = subprocess.check_output(['gcc', '-include', 'errno.h', '-fdirectives-only', '-E', '-xc', '/dev/null'])
         errors = re.findall('^#define (E[A-Z]*) ([0-9]+)', errno.decode(), re.MULTILINE)
-        error = choice(errors)
-        send('#define %s %s' % (error[0], error[1]))
+        errdict = dict((y,x) for x,y in errors)
+        if not msg:
+            msg = choice(list(errdict.keys()))
+        if msg not in errdict:
+            send("%s not found in errno.h" % msg)
+        else:
+            send('#define %s %s' % (errdict[msg], msg))
