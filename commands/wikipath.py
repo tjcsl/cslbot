14c8bc45df11edfd5707ee0b531f8b4e2f9932d9
Peter Foley <pefoley2@verizon.net>
2013-07-28 16:18:46 -0400

    wikipath - fix #32

---
+++ commands/wikipath.py
@@ -0,0 +1,46 @@
+# Copyright (C) 2013 Fox Wilson, Peter Foley, Srijay Kasturi, Samuel Damashek and James Forcier
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+import re
+from datetime import datetime
+from lxml.html import parse
+from urllib.request import urlopen
+from urllib.parse import quote
+from html.parser import HTMLParser
+args = ['modules']
+
+
+def gen_path(msg):
+    epoch = datetime.now().timestamp()
+    url = 'http://beta.degreesofwikipedia.com/?a1=%s&linktype=1&a2=%s&allowsideboxes=1&submit=%s' % (msg[0], msg[1], epoch)
+    html = parse(urlopen(url))
+    path = html.find('body/pre')
+    if not path:
+        return False
+    path = [x.split('=>')[1].strip() for x in path.text.splitlines() if '=>' in x]
+    return " -> ".join(path)
+
+
+def cmd(send, msg, args):
+    msg = msg.split()
+    if len(msg) != 2:
+        send("Need two articles.")
+        return
+    path = gen_path(msg)
+    if path:
+        send(path)
+    else:
+        send("No path found. Do you need to add more links?")
