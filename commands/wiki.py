d46cce1031289d81233cf281e8de0eb1a0659463
Peter Foley <pefoley2@verizon.net>
2013-08-12 19:08:39 -0400

    wiki fixes

--- commands/wiki.py
+++ commands/wiki.py
@@ -20,12 +20,16 @@ from urllib.parse import quote
 
 
 def cmd(send, msg, args):
-        html = urlopen('http://en.wikipedia.org/w/api.php?format=json&action=query&list=search&srlimit=1&srsearch=' + quote(msg))
-        data = json.loads(html.read().decode())
-        try:
-            url = data['query']['search'][0]['title']
-        except Exception:
-            send("%s isn't important enough to have a wikipedia article." % msg)
-            return
-        url = url.replace(' ', '_')
-        send('http://en.wikipedia.org/wiki/' + url)
+    """Returns the first wikipedia result for the argument."""
+    if not msg:
+        send("Need a article.")
+        return
+    html = urlopen('http://en.wikipedia.org/w/api.php?format=json&action=query&list=search&srlimit=1&srsearch=' + quote(msg))
+    data = json.loads(html.read().decode())
+    try:
+        url = data['query']['search'][0]['title']
+    except Exception:
+        send("%s isn't important enough to have a wikipedia article." % msg)
+        return
+    url = url.replace(' ', '_')
+    send('http://en.wikipedia.org/wiki/' + url)
