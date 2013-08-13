82103b70dc2297a746bc781f8fd5b24f131d6976
Peter Foley <pefoley2@verizon.net>
2013-08-13 09:27:04 -0400

    eix imporvements

--- commands/eix.py
+++ commands/eix.py
@@ -19,11 +19,15 @@ import subprocess
 
 
 def cmd(send, msg, args):
-        match = re.match('([A-Za-z0-9][A-Za-z0-9\\-_/]*)', msg)
-        if match:
-                try:
-                    answer = subprocess.check_output(['eix', '-c',
-                                                     match.group(1)])
-                    send(answer.decode().split('\n')[0].rstrip())
-                except subprocess.CalledProcessError:
-                    send(match.group(1) + " isn't important enough for Gentoo.")
+    """Runs eix with the given arguments."""
+    if not msg:
+        send("eix what?")
+        return
+    match = re.match('([A-Za-z0-9][A-Za-z0-9\\-_/]*)', msg)
+    if match:
+            try:
+                answer = subprocess.check_output(['eix', '-c',
+                                                 match.group(1)])
+                send(answer.decode().split('\n')[0].rstrip())
+            except subprocess.CalledProcessError:
+                send(match.group(1) + " isn't important enough for Gentoo.")
