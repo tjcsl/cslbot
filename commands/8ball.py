4dcff4961bfd0b4caeaf9e22dc7ab90c245d0cf5
Peter Foley <pefoley2@verizon.net>
2013-08-13 11:37:26 -0400

    make 8ball do actions

--- commands/8ball.py
+++ commands/8ball.py
@@ -17,12 +17,20 @@
 from random import choice
 from config import NICK
 
+args = ['connection', 'do_log', 'target', 'nick']
+
 
 def cmd(send, msg, args):
-        if not msg:
-            send("What is your question?")
-            return
-        answers = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes, definitely', 'You may rely on it', 'As I see it, yes', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes',
-                   'Reply hazy, try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again',
-                   'Don\'t count on it', 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful']
-        send('%s says... %s' % (NICK, choice(answers)))
+    """Asks the Magic 8-Ball a question.
+    Syntax: !8ball <question>
+    """
+    if not msg:
+        send("What is your question?")
+        return
+    answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes",
+               "Reply hazy, try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again",
+               "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
+    target = args['target'] if args['target'] != 'private' else args['nick']
+    msg = 'says... %s' % choice(answers)
+    args['connection'].action(target, msg)
+    args['do_log'](NICK, msg, 'action')
