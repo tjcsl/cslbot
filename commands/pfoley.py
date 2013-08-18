64495f3ecc5143ce453346e0325bcb2f74ec1f0d
Peter Foley <pefoley2@verizon.net>
2013-08-18 18:01:17 -0400

    pfoley - fwilson = profit!

--- commands/pfoley.py
+++ commands/pfoley.py
@@ -14,8 +14,6 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-from random import random
-
 args = ['modules']
 
 
@@ -25,8 +23,5 @@ def cmd(send, msg, args):
     """
     if not msg:
         msg = args['modules']['word'].gen_word()
-    if random() < 0.5:
-        output = args['modules']['slogan'].gen_slogan(msg)
-    else:
-        output = "fwil" + msg
+    output = args['modules']['slogan'].gen_slogan(msg)
     send(output + " :)")
