a5b4daf81b0b815e7211b5a8d98f6868c2d19c82
csssuf <csssuf@shells.ohai.su>
2013-08-21 00:15:58 -0400

    cleanup !shibe

--- commands/shibe.py
+++ commands/shibe.py
@@ -1,26 +1,21 @@
-from random import random
-from random import choice
-args = ['nick', 'channels', 'target', 'config']
+from random import random, choice
+args = ['nick']
 
 def cmd(send, msg, args):
     """Generates a shibe reaction.
-Syntax: !shibe <topic> or !shibe <topic1> <topic2>
-"""
+    Syntax: !shibe <topic> or !shibe <topic1> <topic2>
+    """
     topics = msg.split(' ')
     if topics.length == 1:
-      topics.append(args['nick'])
-    
+        topics.append(args['nick'])
     send('wow')
-    
     if random() < 0.5:
-      send('so' + topics[0])
-      send('such' + topics[1])
+        send('        so' + topics[0])
+        send('    such' + topics[1])
     else:
-      send('such' + topics[0])
-      send('so' + topics[1])
+        send('        such' + topics[0])
+        send('  so' + topics[1])
     
-    quotes = ['omg', 'amaze', 'nice']
+    quotes = ['omg', 'amaze', 'nice', 'clap']
     send(choice(quotes))
-    
-    send('wow')
-    send('http://i.imgur.com/IEIMlEK.jpg')
+    send('           wow')
