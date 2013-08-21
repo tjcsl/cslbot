0b4047fc5c1f09c22063edf309c4b8d59aa685b8
Anonymous Pineapple <zealan@gmail.com>
2013-08-21 00:19:25 -0400

    Adding no-args scenario
    wow
    such arguments
    so python
    omg
    wow

--- commands/shibe.py
+++ commands/shibe.py
@@ -1,21 +1,27 @@
-from random import random, choice
-args = ['nick']
+from random import random
+from random import choice
+args = ['nick', 'channels', 'target', 'config']
 
 def cmd(send, msg, args):
     """Generates a shibe reaction.
-    Syntax: !shibe <topic> or !shibe <topic1> <topic2>
-    """
+Syntax: !shibe or !shibe <topic> or !shibe <topic1> <topic2>
+"""
     topics = msg.split(' ')
     if topics.length == 1:
-        topics.append(args['nick'])
-    send('wow')
-    if random() < 0.5:
-        send('        so' + topics[0])
-        send('    such' + topics[1])
-    else:
-        send('        such' + topics[0])
-        send('  so' + topics[1])
+      topics.append(args['nick'])
     
-    quotes = ['omg', 'amaze', 'nice', 'clap']
+    send('wow')
+    
+    if topics.length != 0:
+        if random() < 0.5:
+            send('so' + topics[0])
+            send('such' + topics[1])
+        else:
+            send('such' + topics[0])
+            send('so' + topics[1])
+    
+    quotes = ['omg', 'amaze', 'nice']
     send(choice(quotes))
-    send('           wow')
+    
+    send('wow')
+    send('http://i.imgur.com/IEIMlEK.jpg')
