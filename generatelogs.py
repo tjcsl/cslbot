87de4db465403262bd43c9cb693ba3abd1f5034e
Peter Foley <pefoley2@verizon.net>
2013-09-15 15:53:00 -0400

    fix path

--- generatelogs.py
+++ generatelogs.py
@@ -72,7 +72,7 @@ def gen_log(row):
 
 
 def main(config, outdir):
-    dbname = dirname(__file__) + "/log.sqlite"
+    dbname = dirname(__file__) + "/db.sqlite"
     db = sqlite3.connect(dbname)
     db.row_factory = sqlite3.Row
     cursor = db.cursor()
