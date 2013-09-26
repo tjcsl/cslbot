da17bf74afe9bc680950dfda9f73061fca0be5fc
bobtwinkles <srkoser+GitHub@gmail.com>
2013-09-26 18:16:44 -0400

    Made hooks work with the new modules structure and added argument support to hooks

--- modules/__init__.py
+++ modules/__init__.py
@@ -12,11 +12,3 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-
-from modules.hook import Hook
-
-
-@Hook(types=['pubnotice'], reqargs=['nick'])
-def handle(send, args):
-    send("MSBOB ATTACKS %S!" % (args['nick']))
