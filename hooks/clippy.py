69b5254180845a8d0ecb920d7cc369f593fcd946
bobtwinkles <srkoser+GitHub@gmail.com>
2013-09-29 15:10:04 -0400

    Implements a hook that emulates MSClippy, disabled by default

---
+++ hooks/clippy.py
@@ -0,0 +1,28 @@
+# Copyright (C) 2013 Fox Wilson, Peter Foley, Srijay Kasturi, Samuel Damashek, James Forcier and Reed Koser
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+from helpers.hook import Hook
+from commands.clippy import gen_clippy
+from random import random
+
+
+@Hook(types=['pubmsg'], args=['nick', 'config'])
+def handle(send, msg, args):
+    if args['config']['feature']['clippy'] == 'False':
+        return
+    if msg and len(msg.split()) == 1 and random() < 0.005:
+        output = gen_clippy(args['nick'], msg)
+        send(output)
