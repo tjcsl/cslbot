6a47d94fef061b154bb15feeb479fbf57ea38055
bobtwinkles <srkoser+GitHub@gmail.com>
2013-10-09 20:15:48 -0400

    Revert the new babble code, it has been moved to
    !badble or !ababble

--- commands/babble.py
+++ commands/babble.py
@@ -4,6 +4,7 @@
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -14,18 +15,14 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 import re
-from random import choice, randint
+from random import choice
 from helpers.command import Command
-
-#We need to know what can end a sentence
-_sentence_ends = ".!?"
 
 
 # Make the generated messages look better.
 def clean_msg(msg):
     msg = msg.replace('"', '')
     msg = [x for x in msg.split() if not re.match('https?://', x)]
-    msg = [x.strip() for x in msg]
     return msg
 
 
@@ -42,7 +39,6 @@ def weighted_rand(d):
     l = []
     for k in d:
         l += [k] * d[k]
-    print("Choosing from", l)
     return choice(l)
 
 
@@ -52,20 +48,11 @@ def get_messages(cursor, speaker, cmdchar):
     return cursor.execute(query, (speaker,)).fetchall()
 
 
-def update_freq_map(prefix, suffix, m):
-    if prefix not in m:
-        m[prefix] = {}
-    if suffix not in m[prefix]:
-        m[prefix][suffix] = 1
-    else:
-        m[prefix][suffix] += 1
-
-
 #FIXME: make sphinx happy
-def analyze_chat(messages, speaker):
+def build_markov(messages, speaker):
     """ Builds a markov dictionary of the form
 
-        word_list : {
+        word : {
            nextword1 : num_apperences,
            nextword2 : num_apperances,
            ....
@@ -73,64 +60,32 @@ def analyze_chat(messages, speaker):
         }
     """
     markov = {}
-    starts = {}
-    ends = {}
     if messages is None or len(messages) == 0:
-        return ({}, {}, {})
+        return markov
     for msg in messages:
         msg = clean_msg(msg['msg'])
-        #Make sure it's a real message
-        if len(msg) == 0:
-            continue
-        if len(msg) < 4:
-            continue
-        print(msg)
-        start = True
-        while len(msg) > 2:
-            prefix = msg[0:2]
-            prefix_str = " ".join(prefix)
-            suffix = msg[2]
-            if start:
-                update_freq_map(prefix_str, suffix, starts)
-                start = False
-            elif suffix[-1] in _sentence_ends:
-                update_freq_map(prefix_str, suffix, ends)
-            update_freq_map(prefix_str, suffix, markov)
-            msg = msg[1:]
-    return (starts, ends, markov)
+        for i in range(1, len(msg)):
+            if msg[i - 1] not in markov:
+                markov[msg[i - 1]] = {}
+            if msg[i] not in markov[msg[i - 1]]:
+                markov[msg[i - 1]][msg[i]] = 1
+            else:
+                markov[msg[i - 1]][msg[i]] += 1
+    return markov
 
 
-def build_msg(markov, starts, ends, speaker):
+def build_msg(markov, speaker):
     if len(markov.keys()) == 0:
         return "No one has talked with %s =(" % speaker
-    count = 0
-    mwords = randint(7, 20)
-    prefix = choice(list(starts.keys()))
-    suffix = weighted_rand(markov[prefix])
-    out = str(prefix)  # Copy prefix
-    print("Outputting")
-    while count < mwords:
-        print("out       ", out)
-        if count > mwords - 4 and suffix[-1] in _sentence_ends:
+    msg = choice(list(markov.keys()))
+    last_word = msg
+    while len(msg) < 100:
+        if last_word not in markov.keys() or len(list(markov[last_word])) == 0:
             break
-
-        if count > mwords - 4 and suffix[-1] == ',':
-            mwords = count + 6
-
-        if count > mwords - 3 and suffix[-1] not in _sentence_ends:
-            mwords += 1
-
-        print("suffix    ", suffix)
-        if suffix is not None:
-            out = out + " " + suffix
-        prefix = " ".join(out.split()[-2:])
-        #Clearly, we have reached the end of what we can do
-        if prefix not in markov:
-            break
-        print("prefix    ", prefix)
-        suffix = weighted_rand(markov[prefix])
-        count += 1
-    return speaker + " says: " + out
+        next_word = weighted_rand(markov[last_word])
+        msg = "%s %s" % (msg, next_word)
+        last_word = next_word
+    return "%s says: %s" % (speaker, msg)
 
 
 @Command('babble', ['db', 'config'])
@@ -141,5 +96,5 @@ def cmd(send, msg, args):
     cursor = args['db']
     speaker = msg.split()[0] if msg else args['config']['core']['channel']
     messages = get_messages(cursor, speaker, args['config']['core']['cmdchar'])
-    starts, ends, markov = analyze_chat(messages, speaker)
-    send(build_msg(markov, starts, ends, speaker))
+    markov = build_markov(messages, speaker)
+    send(build_msg(markov, speaker))
