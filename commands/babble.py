f7b63a402a57e725fcc5c5b61f2fdf5e457c2ec1
Peter Foley <pefoley2@verizon.net>
2013-10-20 13:10:43 -0400

    babble fixes

--- commands/babble.py
+++ commands/babble.py
@@ -44,7 +44,7 @@ def weighted_rand(d):
 
 def get_messages(cursor, speaker, cmdchar):
     location = 'target' if speaker.startswith('#') else 'source'
-    query = "SELECT msg FROM log WHERE type='pubmsg' OR type='privmsg' AND UPPER(%s)=UPPER(?) AND msg NOT LIKE '%s%%' AND msg NOT LIKE'%%:%%' ORDER BY RANDOM()" % (location, cmdchar)
+    query = "SELECT msg FROM log WHERE (type='pubmsg' OR type='privmsg') AND UPPER(%s)=UPPER(?) AND msg NOT LIKE '%s%%' AND msg NOT LIKE'%%:%%' ORDER BY RANDOM()" % (location, cmdchar)
     return cursor.execute(query, (speaker,)).fetchall()
 
 
