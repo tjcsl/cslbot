efbf02f327e3ad426ac4a10fafa86f9617c9b2e6
Peter Foley <pefoley2@verizon.net>
2013-10-27 21:15:55 -0400

    bingo fixes

--- commands/bingo.py
+++ commands/bingo.py
@@ -17,15 +17,19 @@
 from random import choice
 from helpers.command import Command
 
+
 @Command('bingo', ['nick', 'connection'], limit=2)
 def cmd(send, msg, args):
+    """Creates a bingo board.
+    Syntax: !bingo
+    """
     spaces = ['"Still Alive" on piano', 'polo shirt', 'workstation broken', 'man man', 'csssuf on deadtom',
               '"oooh shiny"', 'cookie clicker', 'everyone is male', 'fwil-', 'someone hates java',
               'pit stains', 'jcotton\'s ipad', 'diet coke', 'booting itanium', 'ggolla has yogurt',
               'password change', 'jholtom uses music cart', 'khakis', 'zan mods mc', 'fwilson\'s stuff stolen',
               'zan feels inferior', 'FOOOOX', 'crashing the san', 'jholtom\'s plaid collared shirt', 'frozen pizza',
               'iodine breaks', 'mac in machine room', 'hipster web tech', 'ethanicorn', 'marching band',
-              'ahamilto\'s laugh', '<creffett> zan --', 'xscreensaver bug', 'shibe', 'no shibing', '<skasturi> hi zan']
+              'ahamilto\'s laugh', '<creffett> POTATO!!!', 'xscreensaver bug', 'shibe', 'no shibing', '<skasturi> hi zan']
 
     board = [[None for i in range(5)] for j in range(5)]
     for i in range(5):
@@ -36,17 +40,13 @@ def cmd(send, msg, args):
     board[2][2] = 'free'
 
     lengths = [None for i in range(5)]
-    for i in range(len(lengths)):
+    for i in range(5):
         lengths[i] = getLongestInColumn(board, i)
 
     for i in range(len(board)):
         for j in range(len(board[0])):
             while len(board[i][j]) < getLongestInColumn(board, j):
                 board[i][j] += ' '
-
-    c = args['connection']
-    nick = args['nick']
-
     send('TJ SysLab Bingo', target=args['nick'])
 
     for i in range(len(board)):
@@ -55,13 +55,13 @@ def cmd(send, msg, args):
             row += board[i][j] + " "
         send(row, target=args['nick'])
 
-    send('------------------------------', target=args['nick'])
+    send('---------------------------------------------------------------------', target=args['nick'])
     send('Make yourself a real bingo board here: http://i.imgur.com/0Z3dxKw.png', target=args['nick'])
 
 
 def getLongestInColumn(array, column):
-    max = 0
+    maxlen = 0
     for i in range(1, len(array)):
-        if len(array[i][column]) > len(array[max][column]):
-            max = i
-    return len(array[max][column])
+        if len(array[i][column]) > len(array[maxlen][column]):
+            maxlen = i
+    return len(array[maxlen][column])
