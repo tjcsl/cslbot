191583806de614aa5c2763f29fd9db69a28249ba
Peter Foley <pefoley2@verizon.net>
2013-11-01 09:13:53 -0400

    wtf fixes

--- commands/wtf.py
+++ commands/wtf.py
@@ -25,6 +25,6 @@ def cmd(send, msg, args):
     """
     try:
         answer = subprocess.check_output(['wtf', msg], stderr=subprocess.STDOUT)
-        send(answer.decode().replace('\n', ' or ').replace('fuck', 'fsck'))
+        send(answer.decode().strip().replace('\n', ' or ').replace('fuck', 'fsck'))
     except subprocess.CalledProcessError as ex:
         send(ex.output.decode().rstrip().splitlines()[0])
