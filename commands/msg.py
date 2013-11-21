24e4172060ed990670921933e4a1f046ee2b0c9b
Srijay Kasturi <skasturi@cadoth.net>
2013-11-21 01:02:27 +0000

    fix #527

---
+++ commands/msg.py
@@ -0,0 +1,31 @@
+# Copyright (C) 2013 Fox Wilson, Peter Foley, Srijay Kasturi, Samuel Damashek, James Forcier and Reed Koser
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+from helpers.command import Command
+import re
+
+@Command('msg',['nick', 'is_admin'])
+def cmd(send, msg, args):
+	"""Sends a message to a channel
+	Syntax: !msg <channel> <message>"""
+	msg = msg.split(maxsplit=1)
+	if re.match("#[^ ,]{1,49}", msg[0]):
+		if args['is_admin'](args['nick']):
+			send(msg[1], target=msg[0])
+		else:
+			send("You are not a admin.", target=args['nick'])
+	else:
+		send("That is not a vaild chan.", target=args['nick'])
