0208f13f62a455faa8312dcdd3283a5be90e94dd
Peter Foley <pefoley2@verizon.net>
2013-12-03 19:07:37 -0500

    timeout

--- helpers/urlutils.py
+++ helpers/urlutils.py
@@ -41,7 +41,7 @@ def get_title(url):
         # Wikipedia doesn't like the default User-Agent, so we rip-off chrome
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.17 Safari/537.36'}
         req = Request(url, headers=headers)
-        html = parse(urlopen(req))
+        html = parse(urlopen(req, timeout=5))
         t = html.find('.//title') if html.getroot() is not None else None
         if t is not None and t.text is not None:
             title = t.text.replace('\n', ' ').strip()
