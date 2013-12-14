3b6658933145c1eae9106ef727910fa52ec4b1e8
fwilson <fwilson@fwilson.me>
2013-12-14 15:23:20 -0500

    repr

--- commands/inspect.py
+++ commands/inspect.py
@@ -27,4 +27,4 @@ def cmd(send, msg, args):
     if not hasattr(handler, msg):
         send("That attribute was not found in the handler.")
         return
-    send(getattr(handler, msg))
+    send(repr(getattr(handler, msg)))
