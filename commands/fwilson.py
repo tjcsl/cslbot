bbd31342f855e6b8c4219214a8cdc0159c8969a8
fwilson <fwilson@fwilson.me>
2013-12-27 18:11:54 -0500

    remove fwilson

--- commands/fwilson.py
+++ commands/fwilson.py
@@ -19,7 +19,7 @@ from helpers.command import Command
 from helpers.textutils import gen_fwilson, gen_word
 
 
-@Command(['fwilson', 'son'])
+@Command(['fwilson'])
 def cmd(send, msg, args):
     """Imitates fwilson.
     Syntax: !fwilson (-f|w) <message>
