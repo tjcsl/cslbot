4c3d5f3ebec8f663bfca4480a784cb1610ced2f4
Peter Foley <pefoley2@verizon.net>
2014-01-10 09:43:56 -0500

    fix #478

--- scripts/parsedata.py
+++ scripts/parsedata.py
@@ -34,20 +34,20 @@ def get_scores(cursor):
     return [dict(row) for row in rows]
 
 
-def get_polls(cursor):
-    rows = cursor.execute('SELECT pid,question FROM polls WHERE deleted=0 AND active=1 ORDER BY pid').fetchall()
-    polls = OrderedDict()
-    for row in rows:
-        polls[row['pid']] = row['question']
-    return polls
-
-
 def get_urls(cursor):
     rows = cursor.execute('SELECT url,title,time FROM urls ORDER BY time DESC').fetchall()
     urls = []
     for row in rows:
         urls.append({'time': datetime.fromtimestamp(row['time']), 'title': row['title'], 'url': row['url']})
     return urls
+
+
+def get_polls(cursor):
+    rows = cursor.execute('SELECT pid,question FROM polls WHERE deleted=0 AND active=1 ORDER BY pid').fetchall()
+    polls = OrderedDict()
+    for row in rows:
+        polls[row['pid']] = row['question']
+    return polls
 
 
 def get_responses(cursor, polls):
@@ -60,6 +60,26 @@ def get_responses(cursor, polls):
                 responses[pid][row['response']] = []
             responses[pid][row['response']].append(row['voter'])
     return responses
+
+
+def get_winners(polls, responses):
+    winners = {}
+    for pid in polls.keys():
+        ranking = {}
+        for response, voters in responses[pid].items():
+            num = len(voters)
+            if num not in ranking:
+                ranking[num] = []
+            ranking[num].append(response)
+        if ranking:
+            high = max(ranking)
+            if len(ranking[high]) == 1:
+                winners[pid] = "The winner is %s with %d votes." % (ranking[high][0], high)
+            else:
+                winners[pid] = "Tie between %s with %d votes." % (", ".join(ranking[high]), high)
+        else:
+            winners[pid] = ""
+    return winners
 
 
 def output_quotes(env, cursor, outdir, time):
@@ -76,7 +96,8 @@ def output_scores(env, cursor, outdir, time):
 
 def output_polls(env, cursor, outdir, time):
     polls = get_polls(cursor)
-    args = {'polls': polls, 'responses': get_responses(cursor, polls), 'time': time}
+    responses = get_responses(cursor, polls)
+    args = {'polls': polls, 'responses': responses, 'winners': get_winners(polls, responses), 'time': time}
     output = env.get_template('polls.html').render(**args)
     open(outdir + '/polls.html', 'w', encoding='utf8').write(output)
 
