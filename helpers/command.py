c932d70fb17cf3ce6930f14505357546b0c00a49
Peter Foley <pefoley2@verizon.net>
2014-01-24 09:37:17 -0500

    fix indexerror

--- helpers/command.py
+++ helpers/command.py
@@ -90,7 +90,7 @@ def check_command(cursor, nick, msg, target):
     limit = datetime.now() - timedelta(seconds=10)
     # the last one is the command we're current executing, so get the penultimate one.
     last = cursor.execute('SELECT msg,source FROM log WHERE target=? AND type="pubmsg" AND time>=? ORDER BY time DESC LIMIT 2', (target, limit.timestamp())).fetchall()
-    if last:
+    if len(last) == 2:
         last = last[1]
         return last['msg'] == msg and last['source'] != nick
     else:
