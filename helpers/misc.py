24b44ae1d8774b0e64dac0dbcabff855b5a8ae15
Peter Foley <pefoley2@pefoley.com>
2014-06-28 22:48:50 -0400

    fix subreddit detection

--- helpers/misc.py
+++ helpers/misc.py
@@ -28,7 +28,7 @@ _pinglist = {}
 
 def check_exists(subreddit):
     req = get('http://www.reddit.com/r/%s/about.json' % subreddit, headers={'User-Agent': 'CslBot/1.0'})
-    if 'data' not in req.json():
+    if req.json()['kind'] == 'Listing':
         # no subreddit exists, search results page is shown
         return False
     return req.status_code == 200
