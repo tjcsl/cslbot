3db921be9b07663186abb3333e965ad7172a6f6c
Anonymous Pineapple <zealan@gmail.com>
2014-08-09 22:12:02 -0400

    Create compare.py
    For Fack2014.

---
+++ commands/compare.py
@@ -0,0 +1,24 @@
+from random import choice
+
+@Command(['compare', 'comparatron'])
+def cmd(send, msg, args):
+    """Compares something.
+    Syntax: !compare (thing)"""
+    
+    comparisons = ["nastier", "whiter", "dumber", "pastier", "crustier", "uglier", "stinkier", "more disgusting", "blacker", "crappier"]
+    superlatives = ["nastiest", "whitest", "dumbest", "pastiest", "crustiest", "ugliest", "stinkiest", "most disgusting", "blackest", "crappiest"]
+    people = ["Maya Angelou", "me", "your momma", "Hillary Clinton", "Thor", "my grandmomma", "you", "the Reverend Martin Luther King, Jr", "the Pope", "Danny Devito", "Sarah Palin"]
+    posessivepeople = ["Maya Angelou's", "my", "yo momma's", "Hillary Clinton's", "Thor's", "my grandmomma's", "your", "the Reverend Martin Luther King, Jr.'s", "the Pope's", "Danny Devito's", "Sarah Palin's"]
+    things = ["vag", "libido", "dingdong", "legs", "sex life", "butt", "body", "torso", "entire extended family", "stupid dumbass face"]
+    
+    mode = random.randint(0, 2)
+    sentence = None
+
+    if mode == 0:
+        sentence = msg + "is " + random.choice(comparisons) + " than " + random.choice(posessivepeople) + " " + random.choice(things) + "."
+    elif mode == 1:
+        sentence = msg + "is " + random.choice(comparisons) + " than " + random.choice(people) + "."
+    elif mode == 2:
+        sentence = msg + "is the " + random.choice(superlatives) + " damn thing."
+    
+    send(sentence)
