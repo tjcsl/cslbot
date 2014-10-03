66e8c00b378fb9889963c2ee29571c622cbd8012
Peter Foley <pefoley2@pefoley.com>
2014-10-03 17:36:17 -0400

    reimplement ctfbot as a hook

---
+++ hooks/ctf.py
@@ -0,0 +1,24 @@
+# Copyright (C) 2013-2014 Fox Wilson, Peter Foley, Srijay Kasturi, Samuel Damashek, James Forcier and Reed Koser
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+import re
+from helpers.hook import Hook
+
+
+@Hook('pubmsg', ['nick'])
+def handle(send, msg, args):
+    if re.match(".*[A-Fa-f0-9]{8}.*", msg):
+        send("%s, sharing flags is against the rules! Please do not share flags during the competition." % args['nick'])
