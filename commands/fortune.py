5889f91a919cc36884101720486a82baf14daa68
Peter Foley <pefoley2@pefoley.com>
2014-11-18 16:13:56 -0500

    fortune improvements

--- commands/fortune.py
+++ commands/fortune.py
@@ -20,38 +20,41 @@ from random import random
 from helpers.command import Command
 
 
-def get_list():
-    output = subprocess.check_output(['fortune', '-f'], stderr=subprocess.STDOUT).decode()
-    output = re.sub('[0-9]\.[0-9]{2}%', '', output)
+def get_list(offensive=False):
+    cmd = ['fortune', '-f']
+    if offensive:
+        cmd.append('-o')
+    output = subprocess.check_output(cmd, stderr=subprocess.STDOUT).decode()
+    output = re.sub('[0-9]{1,2}\.[0-9]{2}%', '', output)
     fortunes = [x.strip() for x in output.splitlines()[1:]]
-    return sorted(fortunes)
+    return set(fortunes)
 
 
 @Command(['fortune', 'bofh', 'excuse'], ['name'])
 def cmd(send, msg, args):
     """Returns a fortune.
-    Syntax: !fortune <list|module|-a|-o>
+    Syntax: !fortune <list|(-a|-o) (module)>
     """
-    try:
-        fortunes = get_list()
-        if msg == 'list':
-            send(" ".join(fortunes))
-        else:
-            if 'bofh' in args['name'] or 'excuse' in args['name']:
-                if random() < 0.05:
-                    send("BOFH Excuse #1337:")
-                    send("You don't exist, go away!")
-                    return
-                mod = 'bofh-excuses'
-            elif msg in fortunes:
-                mod = msg
-            elif not msg or msg == '-o' or msg == '-a':
-                mod = msg
-            else:
-                send("%s is not a valid fortune module" % msg)
+    fortunes = get_list().union(get_list(True))
+    if msg == 'list':
+        send(" ".join(sorted(fortunes)))
+    else:
+        cmd = ['fortune', '-s']
+        match = re.match('(-[ao])( .+|$)', msg)
+        if match:
+            cmd.append(match.group(1))
+            msg = match.group(2).strip()
+        if 'bofh' in args['name'] or 'excuse' in args['name']:
+            if random() < 0.05:
+                send("BOFH Excuse #1337:")
+                send("You don't exist, go away!")
                 return
-            output = subprocess.check_output(['fortune', '-s', mod]).decode()
-            for line in output.splitlines():
-                send(line)
-    except subprocess.CalledProcessError:
-        send("fortune-mod is not installed!")
+            cmd.append('bofh-excuses')
+        elif msg in fortunes:
+            cmd.append(msg)
+        elif msg:
+            send("%s is not a valid fortune module" % msg)
+            return
+        output = subprocess.check_output(cmd).decode()
+        for line in output.splitlines():
+            send(line)
