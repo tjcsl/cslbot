c6231381ea81c5d26f1872ff4144f134a0c4a048
Samuel Damashek <samuel.damashek@gmail.com>
2015-03-16 20:13:45 -0400

    Add autodeop

---
+++ hooks/autodeop.py
@@ -0,0 +1,33 @@
+# Copyright (C) 2013-2015 Fox Wilson, Peter Foley, Srijay Kasturi, Samuel Damashek, James Forcier and Reed Koser
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+from helpers.hook import Hook
+import re
+
+
+@Hook(['pubmsg','action','mode'], ['config','target','type','handler'])
+def handle(send, msg, args):
+    if 'autodeop' not in args['config']['core']:
+        return
+
+    if args['type'] == 'mode':
+        for nick in args['config']['core']['autodeop'].replace(' ','').split(','):
+            if re.match(r'^\+[^ ]*o.+%s.*$'%nick, msg):
+                args['handler'].connection.mode(args['target'], '-o %s' % nick)
+    else:
+        for nick in args['config']['core']['autodeop'].replace(' ','').split(','):
+            if nick in self.channels[args['target']].opers():
+                args['handler'].connection.mode(args['target'], '-o %s' % nick)
