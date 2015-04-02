f62afe1d4f181d389782a0086320f74101cee088
Chris Reffett <cmreffett@gmail.com>
2015-04-02 09:57:34 -0400

    Add optional parameter to !mode to specify the target channel, fixes issue 854

--- commands/mode.py
+++ commands/mode.py
@@ -14,25 +14,35 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+import re
 from helpers.command import Command
 
 
 def set_mode(args, mode):
+    chanmatch = re.match('^#\w+', mode)
+    if chanmatch:
+        target = chanmatch.group(0)
+        # Remove the channel name from the mode string
+        mode = mode.split(' ', 1)[1]
+    else:
+        target = args['target']
     if not mode:
         return "What mode?"
-    if args['target'] == 'private':
+    if target == 'private':
         return "Modes don't work in a PM!"
     if not args['is_admin'](args['nick']):
         return "Admins only"
-    if args['botnick'] not in list(args['handler'].channels[args['target']].opers()):
-        return "Bot must be opped"
-    args['handler'].connection.mode(args['target'], " %s" % mode)
+    if target not in args['handler'].channels:
+        return "Bot not in channel " + target
+    if args['botnick'] not in list(args['handler'].channels[target].opers()):
+        return "Bot must be opped in channel " + target
+    args['handler'].connection.mode(target, " %s" % mode)
     return ""
 
 
 @Command('mode', ['nick', 'is_admin', 'handler', 'botnick', 'target'])
 def cmd(send, msg, args):
     """Sets a mode.
-    Syntax: !mode <mode>
+    Syntax: !mode [channel] <mode>
     """
     send(set_mode(args, msg))
