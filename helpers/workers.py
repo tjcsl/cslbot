6c4c9f345c1d2eb5420f4e2cc6e664b41afe9f49
Peter Foley <pefoley2@pefoley.com>
2015-04-02 23:17:32 -0400

    downgrade exception to message

--- helpers/workers.py
+++ helpers/workers.py
@@ -101,7 +101,7 @@ class Workers():
         ctrlchan = handler.config['core']['ctrlchan']
         with handler.db.session_scope() as session:
             if not update_markov(session, handler.config):
-                raise Exception("Failed to update markov.")
+                send("Failed to update markov, babble may not be up-to-date for the subsequent check.", target=ctrlchan)
             last = session.query(Babble_last).first()
             row = session.query(Log).filter(or_(Log.type == 'pubmsg', Log.type == 'privmsg'), ~Log.msg.startswith(cmdchar), Log.target != ctrlchan).order_by(Log.id.desc()).first()
             if last is None or row is None:
