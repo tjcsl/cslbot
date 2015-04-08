c0aa73f9ee64b72605758421019cc97b3d01e57a
Peter Foley <pefoley2@pefoley.com>
2015-04-08 11:08:28 -0400

    automatically build docs, fix #922

---
+++ scripts/deploy_docs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+  echo "Pushing docs to Github Pages"
+
+  git config --global user.email "tjhsstBot@tjhsst.edu"
+  git config --global user.name "tjhsstBot"
+
+  git clone --depth=50 --branch=gh-pages https://${GH_TOKEN}@github.com/tjcsl/cslbot.git gh-pages
+  rm -rf gh-pages/*
+  cd gh-pages
+  cp -r docs/build/* .
+  git add -A .
+  git commit -m "Update docs, build $TRAVIS_BUILD_NUMBER"
+  git push origin gh-pages && echo "Pushed docs to Github Pages" || echo "Failed to push docs to Github Pages"
+fi
--- scripts/gendocs.sh
+++ scripts/gendocs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd $(dirname $0)/..
 sphinx-apidoc -H IrcBot -f -o doc .
-sphinx-build -b html doc gh-pages
+sphinx-build -W doc doc/build
