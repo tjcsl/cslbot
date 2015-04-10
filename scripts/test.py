2aec8096214d14f5cbad31137918b1e1152f2c0c
Peter Foley <pefoley2@pefoley.com>
2015-04-10 16:19:42 -0400

    add a (currently broken) reload test

--- scripts/test.py
+++ scripts/test.py
@@ -21,17 +21,18 @@ from os.path import dirname, join
 import configparser
 import importlib
 import sys
+import socket
 import irc.client
 
 # FIXME: sibling imports
 sys.path.append(dirname(__file__) + '/..')
 
 
+@mock.patch.object(irc.client.Reactor, 'process_forever')
 class BotTest(unittest.TestCase):
 
-    @mock.patch.object(irc.client.Reactor, 'process_forever')
-    def test_bot_init(self, *args):
-        """Make sure the bot starts up correctly."""
+    @classmethod
+    def setUpClass(cls):
         bot_mod = importlib.import_module('bot')
         botconfig = configparser.ConfigParser()
         configfile = join(dirname(__file__), '../config.cfg')
@@ -40,10 +41,35 @@ class BotTest(unittest.TestCase):
         port = botconfig.getint('core', 'serverport')
         # Avoid port conflicts with running bot
         botconfig['core']['serverport'] = str(port + 1000)
-        bot = bot_mod.IrcBot(botconfig)
-        bot.start()
-        bot.shutdown_server()
-        bot.shutdown_workers()
+        cls.bot = bot_mod.IrcBot(botconfig)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.bot.shutdown_server()
+        cls.bot.shutdown_workers()
+
+    def test_bot_init(self, *args):
+        """Make sure the bot starts up correctly."""
+        self.bot.start()
+        # FIXME: run some commands or something?
+
+    def test_bot_reload(self, *args):
+        return
+        # FIXME: this needs to run in a seperate thread to work.
+        self.bot.reload_event.wait()
+        sock = socket.socket()
+        port = self.bot.config.getint('core', 'serverport')
+        passwd = self.bot.config['auth']['serverpass']
+        sock.connect(('localhost', port))
+        msg = '%s\nreload' % passwd
+        sock.send(msg.encode())
+        output = []
+        while len(output) < 2:
+            resp = sock.recv(4096)
+            output.append(resp)
+        sock.close()
+        output = "".join([x.decode() for x in output])
+        self.assertEqual(output, 'Aye')
 
 if __name__ == '__main__':
     unittest.main()
