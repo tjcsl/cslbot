9aeadc177cd4a1ef93de9e39046882e44258bf87
Peter Foley <pefoley2@pefoley.com>
2015-04-21 15:52:21 -0400

    hopefully actually fix docs this time

--- scripts/deploy_docs.sh
+++ scripts/deploy_docs.sh
@@ -6,16 +6,16 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; th
   git config --global user.name "tjhsstBot"
 
   git clone --depth=50 --branch=gh-pages https://${GH_TOKEN}@github.com/tjcsl/cslbot.git gh-pages
-  latest=$(git log -1 --pretty=%s|sed "s/Update docs, build \([0-9]\+\)/\1/")
-  if [ "$latest" -gt "$TRAVIS_BUILD_NUMBER" ]; then
-      echo "Not overwriting newer docs."
-      exit
-  fi
   rm -rf gh-pages/*
   cd gh-pages
   cp -r ../doc/build/* .
   git add -A .
   git commit -m "Update docs, build $TRAVIS_BUILD_NUMBER"
-  git push origin gh-pages |& sed s/${GH_TOKEN}/[secure]/g
-  echo "Pushed docs to Github Pages"
+  latest=$(git log -1 --pretty=%s|sed "s/Update docs, build \([0-9]\+\)/\1/")
+  if [ "$latest" -gt "$TRAVIS_BUILD_NUMBER" ]; then
+      echo "Not overwriting newer docs."
+  else
+      git push origin gh-pages |& sed s/${GH_TOKEN}/[secure]/g
+      echo "Pushed docs to Github Pages"
+  fi
 fi
