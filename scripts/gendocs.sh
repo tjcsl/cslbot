d5fa4c9dc51f62189178436970c838032d88b6f4
Peter Foley <pefoley2@pefoley.com>
2015-04-25 15:34:59 -0400

    fix docs

--- scripts/gendocs.sh
+++ scripts/gendocs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd $(dirname $0)/..
-sphinx-apidoc -H IrcBot -f -o doc .
+sphinx-apidoc -H IrcBot -f -o doc . setup.py
 sphinx-build -W doc doc/build
 if ! [ -z "`git status --porcelain`" ]; then
     echo "Modified files found, did you add a module?"
