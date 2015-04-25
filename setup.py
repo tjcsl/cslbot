393060ae298f006d2583436d03b66e50d12bea89
Peter Foley <pefoley2@pefoley.com>
2015-04-25 15:49:54 -0400

    first pypi release

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     author="The TJHSST Computer Systems Lab",
     author_email="cslbot@pefoley.com",
     url="https://github.com/tjcsl/cslbot",
-    version="0.11",
+    version="0.12",
     license="GPL",
     packages=find_packages(exclude=['commands.tjhsst']),
     test_suite='scripts.test',
