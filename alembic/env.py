18c6f9f0b832e0676fe9d7438bd8b1584b79e5ce
Peter Foley <pefoley2@pefoley.com>
2015-04-27 11:56:58 -0400

    alembic env cleanups

--- alembic/env.py
+++ alembic/env.py
@@ -1,13 +1,28 @@
-from alembic import context
-import configparser
+# Copyright (C) 2013-2015 Samuel Damashek, Peter Foley, James Forcier, Srijay Kasturi, Reed Koser, Christopher Reffett, and Fox Wilson
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 from sys import path
-from sqlalchemy import create_engine
-import logging
 from os.path import exists, dirname, join
-
 # Make this work from git.
 if exists(join(dirname(__file__), '../.git')):
     path.insert(0, join(dirname(__file__), '..'))
+
+import configparser
+import logging
+from alembic import context
+from sqlalchemy import create_engine
 
 from helpers.orm import Base
 
