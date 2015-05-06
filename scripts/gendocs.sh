8ca6fa39a7dd8cf0d8c5f40d2a0f288823807361
Peter Foley <pefoley2@pefoley.com>
2015-05-05 22:19:26 -0400

    improve gendocs error message

--- scripts/gendocs.sh
+++ scripts/gendocs.sh
@@ -5,5 +5,6 @@ sphinx-apidoc -H IrcBot -f -o doc cslbot
 sphinx-build -W doc build/doc
 if ! [ -z "`git status --porcelain`" ]; then
     echo "Modified files found, did you add a module?"
+    git status --porcelain
     exit 1
 fi
