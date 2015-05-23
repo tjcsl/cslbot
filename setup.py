80d8ed7a5211bcab692260b11cd7197f6e4be5c4
Peter Foley <pefoley2@pefoley.com>
2015-05-23 11:39:37 -0400

    version 0.16

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     author="The TJHSST Computer Systems Lab",
     author_email="cslbot@pefoley.com",
     url="https://github.com/tjcsl/cslbot",
-    version="0.15",
+    version="0.16",
     license="GPL",
     zip_safe=False,
     packages=find_packages(exclude=['cslbot.tjhsst']),
