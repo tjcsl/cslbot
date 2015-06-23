1db28e83090b559601ab1113ccd24cab9796c5c5
Srijay Kasturi (TechFilmer) <srijay@techfilmer.com>
2015-06-22 22:18:04 -0400

    that's funny b/c i am a bee
    me too thanks

---
+++ cslbot/hooks/bee.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2013-2015 Samuel Damashek, Peter Foley, James Forcier, Srijay Kasturi, Reed Koser, Christopher Reffett, and Fox Wilson
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+from ..helpers.hook import Hook
+
+
+@Hook('bee', 'pubmsg')
+def handle(send, msg, args):
+    if msg is "that's funny b/c i am a bee":
+        send("me too thanks")
