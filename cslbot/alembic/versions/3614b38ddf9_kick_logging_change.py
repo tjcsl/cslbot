b47873cbc89c66fdd0fc62e0660e2b7beb9c5d49
Peter Foley <pefoley2@pefoley.com>
2015-09-02 20:23:03 -0400

    Revert "actually migrate properly"
    Turns out this doesn't deal well with existant databases.
    
    This reverts commit 397cfe9eac8d69d9f6a8f716ee70638868912171.

--- cslbot/alembic/versions/3614b38ddf9_kick_logging_change.py
+++ cslbot/alembic/versions/3614b38ddf9_kick_logging_change.py
@@ -8,7 +8,7 @@ Create Date: 2015-04-23 15:55:07.747669
 
 # revision identifiers, used by Alembic.
 revision = '3614b38ddf9'
-down_revision = '37f64e16a31'
+down_revision = 'ef7ccebeff'
 branch_labels = None
 depends_on = None
 
@@ -28,7 +28,7 @@ def upgrade():
 
 def downgrade():
     # FIXME: this adds extraneous commas
-    raise Exception('FIXME')
+    return
     log = sa.table('log', sa.column('type', sa.String), sa.column('msg', sa.String))
     rows = op.get_bind().execute(log.select().where(log.c.type == 'kick')).fetchall()
     rows = [x for x in rows if ',' not in x.msg or x.msg.find(' ') < x.msg.find(',')]
