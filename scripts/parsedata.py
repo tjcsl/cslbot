ecbb0cfc0256d21d662f7276e329d31c5a6ef5c5
Peter Foley <pefoley2@pefoley.com>
2015-11-08 20:02:39 -0500

    sort scores moar

--- scripts/parsedata.py
+++ scripts/parsedata.py
@@ -40,7 +40,7 @@ def get_quotes(session):
 
 
 def get_scores(session):
-    return session.query(Scores).order_by(Scores.nick).all()
+    return session.query(Scores).order_by(Scores.count).all()
 
 
 def get_urls(session):
