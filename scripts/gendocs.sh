b8ad402ced4d5ecd170f6e5a26a961964709e26e
Peter Foley <pefoley2@pefoley.com>
2015-12-30 15:29:53 -0500

    canonical doc build dir

--- scripts/gendocs.sh
+++ scripts/gendocs.sh
@@ -2,7 +2,7 @@
 set -e
 cd $(dirname $0)/..
 sphinx-apidoc -H IrcBot -f -o doc cslbot
-sphinx-build -W doc build/doc
+sphinx-build -W doc -d build/sphinx/doctrees build/sphinx/html
 if ! [ -z "`git status --porcelain`" ]; then
     echo "Modified files found, did you add a module?"
     git status --porcelain
