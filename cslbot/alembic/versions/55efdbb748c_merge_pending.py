2fa8c3452e505439b60c013d8d70c010a6a20f5f
Chris Reffett <cmreffett@gmail.com>
2016-01-23 13:11:28 -0500

    Change column rename in alembic to batch-style add/remove to work for sqlite

--- cslbot/alembic/versions/55efdbb748c_merge_pending.py
+++ cslbot/alembic/versions/55efdbb748c_merge_pending.py
@@ -9,6 +9,8 @@ Create Date: 2015-09-02 13:35:57.956516
 
 from alembic import op
 
+import sqlalchemy as sa
+
 # revision identifiers, used by Alembic.
 revision = '55efdbb748c'
 down_revision = '385c29e42d9'
@@ -17,8 +19,18 @@ depends_on = None
 
 
 def upgrade():
-    op.alter_column('quotes', 'approved', new_column_name='accepted')
+    if op.get_bind().dialect.name == 'sqlite':
+        with op.batch_alter_table('quotes') as batch_op:
+            batch_op.add_column(sa.Column('accepted', sa.Integer(), nullable=True))
+            batch_op.drop_column('approved')
+    else:
+        op.alter_column('quotes', 'approved', new_column_name='accepted')
 
 
 def downgrade():
-    op.alter_column('quotes', 'accepted', new_column_name='approved')
+    if op.get_bind().dialect.name == 'sqlite':
+        with op.batch_alter_table('quotes') as batch_op:
+            batch_op.add_column(sa.Column('approved', sa.Integer(), nullable=True))
+            batch_op.drop_column('accepted')
+    else:
+        op.alter_column('quotes', 'accepted', new_column_name='approved')
