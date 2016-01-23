478fb5e8214ccd9bee7b00d8926dc4101dbbe88b
Chris Reffett <cmreffett@gmail.com>
2016-01-23 13:34:43 -0500

    Make 4d165186b4ed_fix_nullable.py work with sqlite

--- cslbot/alembic/versions/4d165186b4ed_fix_nullable.py
+++ cslbot/alembic/versions/4d165186b4ed_fix_nullable.py
@@ -20,30 +20,60 @@ depends_on = None
 
 
 def upgrade():
-    op.alter_column('babble', 'key',
-                    existing_type=sa.VARCHAR(length=512),
-                    nullable=True)
-    op.alter_column('babble', 'source',
-                    existing_type=sa.TEXT(),
-                    nullable=True)
-    op.alter_column('babble', 'target',
-                    existing_type=sa.TEXT(),
-                    nullable=True)
-    op.alter_column('babble', 'word',
-                    existing_type=sa.TEXT(),
-                    nullable=True)
+    if op.get_bind().dialect.name == 'sqlite':
+        with op.batch_alter_table('babble') as batch_op:
+            batch_op.alter_column('key',
+                                  existing_type=sa.VARCHAR(length=512),
+                                  nullable=True)
+            batch_op.alter_column('source',
+                                  existing_type=sa.TEXT(),
+                                  nullable=True)
+            batch_op.alter_column('target',
+                                  existing_type=sa.TEXT(),
+                                  nullable=True)
+            batch_op.alter_column('word',
+                                  existing_type=sa.TEXT(),
+                                  nullable=True)
+    else:
+        op.alter_column('babble', 'key',
+                        existing_type=sa.VARCHAR(length=512),
+                        nullable=True)
+        op.alter_column('babble', 'source',
+                        existing_type=sa.TEXT(),
+                        nullable=True)
+        op.alter_column('babble', 'target',
+                        existing_type=sa.TEXT(),
+                        nullable=True)
+        op.alter_column('babble', 'word',
+                        existing_type=sa.TEXT(),
+                        nullable=True)
 
 
 def downgrade():
-    op.alter_column('babble', 'word',
-                    existing_type=sa.TEXT(),
-                    nullable=False)
-    op.alter_column('babble', 'target',
-                    existing_type=sa.TEXT(),
-                    nullable=False)
-    op.alter_column('babble', 'source',
-                    existing_type=sa.TEXT(),
-                    nullable=False)
-    op.alter_column('babble', 'key',
-                    existing_type=sa.VARCHAR(length=512),
-                    nullable=False)
+    if op.get_bind().dialect.name == 'sqlite':
+        with op.batch_alter_table('babble') as batch_op:
+            batch_op.alter_column('word',
+                                  existing_type=sa.TEXT(),
+                                  nullable=False)
+            batch_op.alter_column('target',
+                                  existing_type=sa.TEXT(),
+                                  nullable=False)
+            batch_op.alter_column('source',
+                                  existing_type=sa.TEXT(),
+                                  nullable=False)
+            batch_op.alter_column('key',
+                                  existing_type=sa.VARCHAR(length=512),
+                                  nullable=False)
+    else:
+        op.alter_column('babble', 'word',
+                        existing_type=sa.TEXT(),
+                        nullable=False)
+        op.alter_column('babble', 'target',
+                        existing_type=sa.TEXT(),
+                        nullable=False)
+        op.alter_column('babble', 'source',
+                        existing_type=sa.TEXT(),
+                        nullable=False)
+        op.alter_column('babble', 'key',
+                        existing_type=sa.VARCHAR(length=512),
+                        nullable=False)
