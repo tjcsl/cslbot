ba9c2498ad871feb92fe06532bc46dc1573bf10c
Peter Foley <pefoley2@pefoley.com>
2016-01-24 17:53:36 -0500

    cleanup/refactor tests

--- scripts/test.py
+++ scripts/test.py
@@ -17,7 +17,9 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 import configparser
 import logging
+import shutil
 import socket
+import tempfile
 import unittest
 from os.path import dirname, exists, join
 from sys import path
@@ -30,15 +32,7 @@ if exists(join(dirname(__file__), '../.git')):
 
 # Imports pkg_resources, so must come after the path is modified
 import irc.client  # noqa
-from cslbot.helpers import core, server  # noqa
-
-
-def config_mock(config, section, option):
-    ret = int(config[section][option])
-    # Avoid port conflicts with running bot
-    if section == 'core' and option == 'serverport':
-        return ret + 1000
-    return ret
+from cslbot.helpers import core, server, sql  # noqa
 
 
 def connect_mock(conn, *args, **_):
@@ -51,26 +45,49 @@ class BotTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        confdir = join(dirname(__file__), '..')
-        mock.patch.object(configparser.ConfigParser, 'getint', config_mock).start()
         mock.patch.object(irc.client.ServerConnection, 'connect', connect_mock).start()
-        # FIXME: don't request oauth tokens
-        # mock.patch('cslbot.helpers.tokens.update_all_tokens').start()
-        cls.bot = core.IrcBot(confdir)
+        cls.setup_config()
+        cls.bot = core.IrcBot(cls.confdir.name)
         cls.setup_handler()
         # We don't actually connect to an irc server, so fake the event loop
         with mock.patch.object(irc.client.Reactor, 'process_forever'):
             cls.bot.start()
 
+    def join_channel(self, nick):
+        e = irc.client.Event('join', irc.client.NickMask(nick), '#test-channel')
+        self.bot.connection._handle_event(e)
+        calls = [x[0] for x in self.log_mock.call_args_list]
+        expected_calls = [(nick, '#test-channel', 0, '', 'join')]
+        if nick == 'testBot':
+            expected_calls.append((nick, 'private', 0, 'Joined channel #test-channel', 'privmsg'))
+        self.assertEqual(calls, expected_calls)
+        self.log_mock.reset_mock()
+
     @classmethod
     def tearDownClass(cls):
         cls.bot.shutdown_mp()
+        cls.confdir.cleanup()
+
+    @classmethod
+    def setup_config(cls):
+        cls.confdir = tempfile.TemporaryDirectory()
+        srcdir = join(dirname(__file__), '..')
+        shutil.copy(join(srcdir, 'config.cfg'), cls.confdir.name)
+        shutil.copy(join(srcdir, 'groups.cfg'), cls.confdir.name)
+        config_obj = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
+        config_file = join(cls.confdir.name, 'config.cfg')
+        with open(config_file) as f:
+                config_obj.read_file(f)
+        # Prevent server port conflicts
+        config_obj['core']['serverport'] = str(config_obj.getint('core', 'serverport') + 1000)
+        # FIXME: use an in-memory db
+        config_obj['db']['engine'] = 'sqlite:///%s' % join(cls.confdir.name, 'db.sqlite')
+        with open(config_file, 'w') as f:
+                config_obj.write(f)
 
     @classmethod
     def setup_handler(cls):
-        cls.bot.handler.channels = {'#test-channel': mock.MagicMock()}
-        cls.bot.handler.is_ignored = mock.MagicMock(return_value=False)
-        cls.bot.handler.db = mock.MagicMock()
+        cls.log_mock = mock.patch.object(cls.bot.handler.db, 'log').start()
 
     def restart_workers(self):
         """Force all the workers to restart so we get the log message."""
@@ -80,12 +97,14 @@ class BotTest(unittest.TestCase):
 
     def test_handle_msg(self):
         """Make sure the bot can handle a simple message."""
+        self.join_channel('testBot')
         e = irc.client.Event('pubmsg', irc.client.NickMask('testnick'), '#test-channel', ['!morse bob'])
         # We mocked out the actual irc processing, so call the internal method here.
         self.bot.connection._handle_event(e)
         self.restart_workers()
-        calls = [x[0] for x in self.bot.handler.db.log.call_args_list]
+        calls = [x[0] for x in self.log_mock.call_args_list]
         self.assertEqual(calls, [('testnick', '#test-channel', 0, '!morse bob', 'pubmsg'), ('testBot', '#test-channel', 0, '-... --- -...', 'privmsg')])
+        self.log_mock.reset_mock()
 
     def test_bot_reload(self):
         """Make sure the bot can reload without errors."""
