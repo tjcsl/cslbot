619ef9d3bdefd2f698947bbfa199bd561a1e7f0d
Peter Foley <pefoley2@pefoley.com>
2016-02-09 18:25:27 -0500

    pep8

--- cslbot/helpers/textutils.py
+++ cslbot/helpers/textutils.py
@@ -23,11 +23,11 @@ import string
 from html import escape, unescape
 from random import choice, randint, random, randrange
 
+from lxml import html
+
 from pkg_resources import Requirement, resource_string
 
 from requests import get, post
-
-from lxml import html
 
 from textblob import TextBlob
 from textblob.exceptions import NotTranslated
@@ -260,14 +260,14 @@ def gen_translate(msg, fromlang=None, outputlang='en'):
 
 def gen_random_translate(msg):
     languages = [
-                'ko', 'eo', 'la', 'so', 'tg', 'zh', 'st', 'km', 'ja', 'su', 'ny', 'pl', 'gu',
-                'sk', 'bs', 'ur', 'jw', 'si', 'es', 'lt', 'yo', 'sw', 'hu', 'ka', 'be', 'hi',
-                'sv', 'el', 'de', 'ro', 'bn', 'ceb', 'eu', 'nl', 'ig', 'mg', 'te', 'no', 'yi',
-                'ar', 'ca', 'pt', 'uk', 'hr', 'iw', 'th', 'sl', 'et', 'id', 'mn', 'zh-TW', 'ga',
-                'mt', 'sr', 'sq', 'ml', 'zh-CN', 'mi', 'mr', 'cs', 'hy', 'gl', 'cy', 'vi', 'uz',
-                'pa', 'ht', 'is', 'ms', 'af', 'lo', 'ne', 'lv', 'bg', 'fr', 'tr', 'ha', 'mk',
-                'fa', 'it', 'kn', 'az', 'hmn', 'kk', 'my', 'fi', 'zu', 'ru', 'ta', 'da', 'tl'
-                ]
+        'ko', 'eo', 'la', 'so', 'tg', 'zh', 'st', 'km', 'ja', 'su', 'ny', 'pl', 'gu',
+        'sk', 'bs', 'ur', 'jw', 'si', 'es', 'lt', 'yo', 'sw', 'hu', 'ka', 'be', 'hi',
+        'sv', 'el', 'de', 'ro', 'bn', 'ceb', 'eu', 'nl', 'ig', 'mg', 'te', 'no', 'yi',
+        'ar', 'ca', 'pt', 'uk', 'hr', 'iw', 'th', 'sl', 'et', 'id', 'mn', 'zh-TW', 'ga',
+        'mt', 'sr', 'sq', 'ml', 'zh-CN', 'mi', 'mr', 'cs', 'hy', 'gl', 'cy', 'vi', 'uz',
+        'pa', 'ht', 'is', 'ms', 'af', 'lo', 'ne', 'lv', 'bg', 'fr', 'tr', 'ha', 'mk',
+        'fa', 'it', 'kn', 'az', 'hmn', 'kk', 'my', 'fi', 'zu', 'ru', 'ta', 'da', 'tl'
+    ]
     language = choice(languages)
     blob = TextBlob(msg)
     try:
