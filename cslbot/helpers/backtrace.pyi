27845a9bc9617ec0fc6fd2ae61e0d0edde241e33
Peter Foley <pefoley2@pefoley.com>
2016-10-02 18:31:12 -0400

    fixes

--- cslbot/helpers/backtrace.pyi
+++ cslbot/helpers/backtrace.pyi
@@ -1,3 +1,6 @@
-def output_traceback(ex: Exception) -> Tuple[str, str]:
-def handle_traceback(ex: Exception, c: client.ServerConnection, target: str, config: configparser.ConfigParser, source: str) -> None:
-#        def send(_, msg: str) -> None:
+import configparser
+from irc import client
+from typing import Tuple
+
+def output_traceback(ex: Exception) -> Tuple[str, str]: ...
+def handle_traceback(ex: Exception, c: client.ServerConnection, target: str, config: configparser.ConfigParser, source: str) -> None: ...
