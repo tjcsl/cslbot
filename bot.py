2164c5e6e3a1de34215e0edf30bb19154132e6fc
Peter Foley <pefoley2@pefoley.com>
2017-05-20 19:55:53 -0400

    deprecation

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ from os.path import abspath, dirname
 from sys import path
 
 warnings.simplefilter('default')
-warnings.filterwarnings('ignore', category=DeprecationWarning, module='alembic')
+warnings.filterwarnings('ignore', category=DeprecationWarning, module='alembic.*')
 warnings.filterwarnings('ignore', category=DeprecationWarning, module='nltk')
 warnings.filterwarnings('ignore', category=PendingDeprecationWarning, module='nltk')
 
