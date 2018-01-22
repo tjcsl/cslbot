ace2e9b70ff9336c43573645554c1f0bdca5544e
Peter Foley <pefoley2@pefoley.com>
2018-01-21 19:45:55 -0500

    workaround sphinx bug

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 cd $(dirname $0)/..
-sphinx-apidoc -H IrcBot -f -o build/.tmp cslbot
-rsync --checksum build/.tmp/*.rst doc/
+# TODO(pfoley): Figure out why new versions of sphinx drop cslbot.commands.
+# sphinx-apidoc -H IrcBot -f -o build/.tmp cslbot
+# rsync --checksum build/.tmp/*.rst doc/
 sphinx-build -W doc -d build/sphinx/doctrees build/sphinx/html
 if ! [ -z "`git status --porcelain`" ]; then
     echo "Modified files found, did you add a module?"
