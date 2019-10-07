dd9d240bbf650781ad528fa26d05065defff9b5d
Peter Foley <pefoley2@pefoley.com>
2019-10-07 02:12:57 -0400

    strict optional checks

--- scripts/mypy.sh
+++ scripts/mypy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 export MYPYPATH=test/stubs
 echo "Checking static types..."
-env python3 -m mypy .
+env python3 -m mypy --strict \
+    --allow-untyped-defs \
+    --allow-untyped-calls \
+    --allow-incomplete-defs \
+    --allow-any-generics \
+    --no-check-untyped-defs .
