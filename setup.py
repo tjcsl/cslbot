9a8431782d71053055a428edc79ed6987a32f737
Peter Foley <pefoley2@pefoley.com>
2020-05-11 00:24:11 -0400

    pytest

--- setup.py
+++ setup.py
@@ -34,7 +34,6 @@ setup(
     license="GPL",
     zip_safe=False,
     packages=find_packages(exclude=["cslbot.tjhsst"]),
-    test_suite="test.test",
     package_data={
         "": [
             "alembic/env.py",
